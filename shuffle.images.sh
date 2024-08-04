339c5936e1e27ef9784ea11bcb64ece9bdce0672
Alex Elliott <alexpotato@shuttle>
2024-08-04 09:15:25 -0400

    move qr code page generation to it's own code block in shuffle images

--- shuffle.images.sh
+++ shuffle.images.sh
@@ -3,7 +3,6 @@
 ls *.jpeg | sort -R > shuffled.images.txt
 
 echo "<html><body>" > main.html
-echo "<html><body>" > qr.html
 a=1
 for i in $(cat shuffled.images.txt)
 do
@@ -17,11 +16,31 @@ do
     # write to main for index
     echo "<h2>$a $i</h2>" >> main.html
     # generate QR page
-    echo "<div>" >> qr.html
-    echo "<span>$a</span>" >> qr.html
-    echo "<img src='$a.png' style='border: solid 1px black;'><img>" >> qr.html
-    echo "</div>" >> qr.html
     a=$(echo "$a + 1" | bc)    
 done
 echo "</body>" >> main.html
 echo "</html>" >> main.html
+
+images=$(wc -l shuffled.images.txt | cut -d" " -f1)
+echo "Images: $images"
+rows=$(echo "$images / 5" | bc)
+echo "Rows: $rows"
+a=1
+echo "<html><body>" > qr.html
+echo "<table>" >> qr.html
+for r in $(seq 0 $rows)
+do
+    echo "<tr>" >> qr.html
+    for i in $(seq 1 5)
+    do
+        echo "<td style='padding: 5px;'>" >> qr.html
+        echo "<div>" >> qr.html
+        echo "<span>$a</span>" >> qr.html
+        echo "<img src='$a.png' style='border: solid 1px black;'><img>" >> qr.html
+        echo "</div>" >> qr.html
+        echo "</td>" >> qr.html
+        a=$(echo "$a + 1" | bc)    
+    done
+    echo "</tr>" >> qr.html
+done
+echo "</table>" >> qr.html
